72558f26fa88d22d623228bed3af7523f043291c
Mazhar <nsrmazhar@gmail.com>
2022-12-31 15:24:29 +0530

    Smallest & Largest Number

--- LargeSmall.js
+++ LargeSmall.js
@@ -1,3 +1,8 @@
+// Exercise 1: In a computer system, N values are stored in the memory. 
+// A programmer needs to find out the largest and smallest value present in the memory. 
+// But the condition is, only one scan is allowed, multiple scans/passes are not allowed. 
+// Write a program to find the largest and smallest number from the given list of numbers in single pass.
+
 let array = [52, 10, 26, 22, 13, 200]
 
 let small=Number.MAX_SAFE_INTEGER, large=0
